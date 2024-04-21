dcc84a58931e793fca50229afd816ec69839434f
pengjian05 <pengjian05@baidu.com>
2024-04-21 13:37:49 +0800

    Implemented new feature for improved user experience.

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import dotenv
 from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
 from rss_storage import StorageNews
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 dotenv.load_dotenv(dotenv_path='.env')
 app = Flask(__name__)
-print("begin to connect google spread sheet")
+logging.info("begin to connect google spread sheet")
 storage = StorageNews("gs_service_account.json", os.getenv("SHEET_KEY"))
-print("end to connect google spread sheet")
+logging.info("end to connect google spread sheet")
 
 
 def task():
@@ -17,24 +21,25 @@ def task():
         url = f.readline().strip()
         if len(url) > 4:
             urls.append(url)
-    print("feed urls=", urls)
+    logging.info("feed urls=%s", ','.join(urls))
     storage.storage_to_google_spreadsheet(urls)
 
 
 @app.route('/')
 @app.route('/health')
 def health():
+    logging.info("health interface")
     return "ok"
 
 
-if __name__ == '__main__':
-    scheduler = BackgroundScheduler()
-    interval = int(os.getenv("INTERVAL_SECONDS"))
-    print("schedula interval=", interval)
-    scheduler.add_job(task, 'interval', seconds=interval)
-    scheduler.start()
+scheduler = BackgroundScheduler()
+interval = int(os.getenv("INTERVAL_SECONDS"))
+logging.info("schedula interval=%d", interval)
+scheduler.add_job(task, 'interval', seconds=interval)
+scheduler.start()
 
-    try:
-        app.run()
-    finally:
-        scheduler.shutdown()
+
+@app.teardown_appcontext
+def treadown(exception=None):
+    logging.info("teardown the app context of flask")
+    scheduler.shutdown()
